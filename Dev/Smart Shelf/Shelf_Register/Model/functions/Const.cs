2b5eee746730cb5f033d51024a069e5d44442cf1
unknown <thinhngo.tony@gmail.com>
2023-01-03 09:34:18 +0700

    Init - bugging with base64

---
+++ Dev/Smart Shelf/Shelf_Register/Model/functions/Const.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelf_Register
+{
+    public static class Const
+    {
+
+        public static string blank_image = @"Common/images/blank_background.png";
+        public static string no_image = @"Common/images/noimage.png";
+
+    }
+}
---
+++ Dev/Smart Shelf/Shelf_Register/View/Setting.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Shelf_Register
+{
+    partial class Setting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            settingLayer = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // settingLayer
+            // 
+            settingLayer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            settingLayer.ColumnCount = 10;
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            settingLayer.Location = new System.Drawing.Point(0, 0);
+            settingLayer.Name = "settingLayer";
+            settingLayer.RowCount = 5;
+            settingLayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            settingLayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            settingLayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            settingLayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            settingLayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            settingLayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            settingLayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            settingLayer.Size = new System.Drawing.Size(1008, 729);
+            settingLayer.TabIndex = 0;
+            settingLayer.Paint += new System.Windows.Forms.PaintEventHandler(settingLayer_Paint);
+            // 
+            // Setting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(settingLayer);
+            this.Name = "Setting";
+            this.Text = "Setting";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public static System.Windows.Forms.TableLayoutPanel settingLayer;
+    }
+}
